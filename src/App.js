4b00b0bb0c9537fb7da8f42eccfcf643ad310c5a
Dewale <Adewaleademilua@gmail.com>
2022-01-15 21:55:38 +0100

    dropdown implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "tailwindcss/tailwind.css";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   return (
     <div className="App" class="overflow-x-hidden w-screen bg-homeBackgroundMobile bg-cover bg-center sm:bg-homeBackgroundTablet md:bg-homeBackgroundDesktop h-screen flex flex-col justify-between">
       <Nav />
       <Home/>
+      {/* <div class="md:hidden">
+        <Dropdown/>
+      </div> */}
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Dropdown = () => {
     return (
-        <div>
-            
+        <div class="flex list-none w-screen flex-col justify-content items-center bg-white">
+            <li><a href="">HOME</a></li>
+            <li><a href="">DESTINATION</a></li>
+            <li><a href="">CREW</a></li>
+            <li><a href="">TECHNOLOGY</a></li>
         </div>
     )
 }
