b18b22334878a74c5899b8defce5d93ff4e25f2a
Dewale <Adewaleademilua@gmail.com>
2022-01-15 21:13:34 +0100

    responsive homepage

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,50 @@
 import React from "react";
 import img from "../images/favicon-32x32.png";
+import { Spiral as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 const Nav = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div class="flex mt-7 w-screen justify-between items-center">
       <h3>
-        <img src={img} alt="" class="h-12 ml-16" />
+        <img src={img} alt="" class="h-8 ml-16" />
       </h3>
-      <nav>
-        <ul class="flex items-center justify-center pl-24 pr-48 text-white bg-slate-300/50 tracking-wide">
-          <li class="mr-4 border-white border-b-2 py-5"><a href=""><span class="font-bold pb-5">00</span> HOME</a></li>
-          <li class="mr-4 py-5"><a href=""><span class="font-bold">01</span> DESTINATION</a></li>
-          <li class="mr-4 py-5"><a href=""><span class="font-bold">02</span> CREW</a></li>
-          <li class="mr-4 py-5"><a href=""><span class="font-bold">03</span> TECHNOLOGY</a></li>
+      <nav className="hidden md:block">
+        <ul class="flex items-center justify-center pl-24 pr-24 lg:pr-48 text-white bg-slate-300/50 tracking-wide">
+          <li class="mr-4 border-white border-b-2 py-4">
+            <a href="">
+              <span class="font-bold">00</span> HOME
+            </a>
+          </li>
+          <li class="mr-4 py-4">
+            <a href="">
+              <span class="font-bold">01</span> DESTINATION
+            </a>
+          </li>
+          <li class="mr-4 py-4">
+            <a href="">
+              <span class="font-bold">02</span> CREW
+            </a>
+          </li>
+          <li class="mr-4 py-4">
+            <a href="">
+              <span class="font-bold">03</span> TECHNOLOGY
+            </a>
+          </li>
         </ul>
       </nav>
+      <div class="md:hidden mr-16">
+        <Hamburger
+          size={30}
+          direction="right"
+          toggled={isOpen}
+          toggle={setOpen}
+          hideOutline={false}
+          label="Show menu"
+          easing="ease-in"
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,21 +2,23 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div class="text-white m-28">
-      <h3 class="text-2xl uppercase">So, you want to travel to</h3>
-      <div class="flex sm:flex-row flex-col justify-between">
-        <div>
-          <h1 class="text-8xl mb-3 tracking-wider">SPACE</h1>
-          <p class="max-w-md">
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
+    <div class="flex flex-col justify-center items-center mt-20 mb-10 md:my-28 md:block">
+      <div class="text-white md:mx-24">
+        <h3 class="text-lg md:text-2xl uppercase">So, you want to travel to</h3>
+        <div class="flex md:flex-row flex-col justify-center items-center md:justify-between">
+          <div>
+            <h1 class="text-8xl mb-3 tracking-wider">SPACE</h1>
+            <p class="max-w-md">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <h1 class="bg-white flex w-48 h-48 rounded-full justify-center items-center text-blue-900 mt-12 md:mt-0">
+            EXPLORE
+          </h1>
         </div>
-        <h1 class="bg-white flex w-48 h-48 rounded-full justify-center items-center text-blue-900 mt-12 sm:mt-0">
-          EXPLORE
-        </h1>
       </div>
     </div>
   );
